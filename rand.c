26ba04e7c03b35395f7f0fcfb8a5b0c26f48a565
Robert Elia <relia@pdx.edu>
2024-04-17 16:13:01 -0700

    Adding rand

---
+++ rand.c
@@ -0,0 +1,19 @@
+#ifndef RAND_C
+#define RAND_C
+
+#include "rand.h"
+
+static unsigned long next = 1;
+
+int rand(void)
+{
+    next = next * 1103515245 + 12345;
+    return((unsigned)(next/65536) % RAND_MAX);
+}
+
+void srand(unsigned int seed)
+{
+    next = seed;
+}
+
+#endif // RAND_C
---
+++ rand.h
@@ -0,0 +1,11 @@
+#ifndef RAND_H
+#define RAND_H
+
+#ifndef RAND_MAX
+#define RAND_MAX (1 << 31)
+#endif // RAND_MAX
+
+int rand(void);
+void srand(unsigned int seed);
+
+#endif // RAND_H
