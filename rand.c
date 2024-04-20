2e4d7b09eb3ff966cf0a37ce89ca9eccc61859ae
Robert Elia <relia@pdx.edu>
2024-04-19 21:16:48 -0700

    change int to unsigned int

--- rand.c
+++ rand.c
@@ -5,7 +5,7 @@
 
 static unsigned long next = 1;
 
-int rand(void)
+unsigned int rand(void)
 {
     next = next * 1103515245 + 12345;
     return((unsigned)(next/65536) % RAND_MAX);
--- rand.h
+++ rand.h
@@ -5,7 +5,7 @@
 #define RAND_MAX (1 << 31)
 #endif // RAND_MAX
 
-int rand(void);
+unsigned int rand(void);
 void srand(unsigned int seed);
 
 #endif // RAND_H
